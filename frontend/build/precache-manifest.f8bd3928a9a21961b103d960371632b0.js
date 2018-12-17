e03baf81ecfb785a68e0dea886dd3624f1723ad5
cdamose <cdamose@gmail.com>
2018-12-17 23:10:53 +0530

    Initial commit react app

---
+++ frontend/build/precache-manifest.f8bd3928a9a21961b103d960371632b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13536284c7dec7781db5",
+    "url": "/static/js/main.13536284.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "13536284c7dec7781db5",
+    "url": "/static/css/main.2c0a16e9.chunk.css"
+  },
+  {
+    "revision": "5640190e93de4ab42c19317cdf858cf2",
+    "url": "/index.html"
+  }
+];
